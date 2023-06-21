ea1e04db1914d8dce51a6194127cbd92671608e1
Seva <vsevolodchel@gmail.com>
2023-06-21 18:47:24 +0300

    Сохранения 2

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def stringi(s):
+'''def stringi(s):
     for sym in set(s):
         c = 0
         for sub_sym in s:
             if sym == sub_sym:
                 c += 1
         print(sym, c)
-stringi('dwwqwew')
+stringi('dwwqwew')'''
+print('Git')
